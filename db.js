57a11fc7565d74c35f280a03986a4866352c282b
lucassouza-dev13 <la1765035@gmail.com>
2026-04-17 12:28:18 -0300

    erro ipv6

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg");
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
+  family: 4, // 🔥 ISSO AQUI resolve o erro
 });
 
 module.exports = pool;
