3778f4b75af05e40877276ba8e76cd430ab41f9e
lucassouza-dev13 <la1765035@gmail.com>
2026-06-23 15:16:14 -0300

    remove pasta duplicada BackEnd-Cat-logo

--- BackEnd-Cat-logo/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  family: 4,
-});
-
-module.exports = pool;
